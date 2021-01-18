92ed7e811bdda6a815a95acd921359a7e2d2992f
bgscandra <bgs.candra@gmail.com>
2021-01-18 17:07:32 +0700

    Demo API using Spring Boot

--- src/main/java/com/example/demo/api/CustomerResource.java
+++ src/main/java/com/example/demo/api/CustomerResource.java
@@ -3,8 +3,11 @@ package com.example.demo.api;
 import com.example.demo.model.Customer;
 import com.example.demo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
@@ -14,28 +17,43 @@ public class CustomerResource {
     @Autowired
     private CustomerService customerService;
 
+    @Autowired
+    private KafkaTemplate<String, Customer> kafkaTemplate;
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplateString;
+
+    private static final String TOPIC = "KafkaAPI";
+    private static final String USAGE_TOPIC = "KafkaUsageAPI";
+
     @PostMapping
     public Customer addCustomer(@RequestBody Customer customer) {
+        kafkaTemplate.send(TOPIC, customer);
+        kafkaTemplateString.send(USAGE_TOPIC,"POST Customer API has been Accessed");
         return customerService.addCustomer(customer);
     }
 
     @GetMapping
     public List<Customer> getCustomers() {
+        kafkaTemplateString.send(USAGE_TOPIC,"GET Customer API has been Accessed");
         return customerService.getCustomers();
     }
 
     @GetMapping(value = "/{customerId}")
     public Customer getCustomer(@PathVariable("customerId") int customerId) {
+        kafkaTemplateString.send(USAGE_TOPIC,"GET BY ID Customer API has been Accessed");
         return customerService.getCustomer(customerId);
     }
 
     @PutMapping(value = "/{customerId}")
     public Customer updateCustomer(@PathVariable("customerId") int customerId,@RequestBody Customer customer) {
+        kafkaTemplateString.send(USAGE_TOPIC,"PUT Customer API has been Accessed");
         return customerService.updateCustomer(customerId,customer);
     }
 
     @DeleteMapping(value = "/{customerId}")
     public void deleteCustomer(@PathVariable("customerId") int customerId) {
+        kafkaTemplateString.send(USAGE_TOPIC,"DELETE Customer API has been Accessed");
         customerService.deleteCustomer(customerId);
     }
 }
--- src/main/java/com/example/demo/api/MovieResource.java
+++ src/main/java/com/example/demo/api/MovieResource.java
@@ -3,6 +3,7 @@ package com.example.demo.api;
 import com.example.demo.model.Movie;
 import com.example.demo.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,28 +15,38 @@ public class MovieResource {
     @Autowired
     private MovieService movieService;
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplateString;
+
+    private static final String USAGE_TOPIC = "KafkaUsageAPI";
+
     @PostMapping
     public Movie addMovies(@RequestBody Movie movie){
+        kafkaTemplateString.send(USAGE_TOPIC,"POST Movie API has been Accessed");
         return movieService.addMovie(movie);
     }
 
     @GetMapping
     public List<Movie> getMovies() {
+        kafkaTemplateString.send(USAGE_TOPIC,"GET Movie API has been Accessed");
         return movieService.getMovies();
     }
 
     @GetMapping(value = "/{movieId}")
     public Movie getMovie(@PathVariable("movieId") int movieId) {
+        kafkaTemplateString.send(USAGE_TOPIC,"GET BY ID Movie API has been Accessed");
         return movieService.getMovie(movieId);
     }
 
     @PutMapping(value = "/{movieId}")
     public Movie updateMovie(@PathVariable("movieId") int movieId,@RequestBody Movie movie) {
+        kafkaTemplateString.send(USAGE_TOPIC,"PUT Movie API has been Accessed");
         return movieService.updateMovie(movieId,movie);
     }
 
     @DeleteMapping(value = "/{movieId}")
     public void deleteMovie(@PathVariable("movieId") int movieId) {
+        kafkaTemplateString.send(USAGE_TOPIC,"DELETE Movie API has been Accessed");
         movieService.deleteMovie(movieId);
     }
 
